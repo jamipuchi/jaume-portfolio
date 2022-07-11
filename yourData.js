da4a9ac7c9499ad116714fa9f0b9061d09b9ad69
miquel <jaumepuigturon4@gmail.com>
2022-07-11 20:09:15 +0200

    Commit stuff

---
+++ yourData.js
@@ -0,0 +1,123 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Jaume",
+  headerTaglineOne: "Hello 👋",
+  headerTaglineTwo: "I'm Jaume Puig,",
+  headerTaglineThree: "freelance web developer",
+  headerTaglineFour: "- for hire.",
+
+  socials: [
+    { title: "Github", link: "https://github.com/jamipuchi" },
+    {
+      title: "LinkedIn",
+      link: "https://www.linkedin.com/in/jaume-puig-turon/",
+    },
+    { title: "Email", link: "mailto:jaumepuigturon4@gmail.com" },
+  ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Toptal", //Project Title - Add Your Project Title Here
+      description: "Senior fullstack engineer",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://raw.githubusercontent.com/jamipuchi/react-portfolio-template/main/public/images/toptal.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.toptal.com/resume/jaume-puig-turon",
+    },
+    {
+      title: "New Relic", //Project Title - Add Your Project Title Here
+      description: "Front end engineer",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://raw.githubusercontent.com/jamipuchi/react-portfolio-template/main/public/images/new_relic.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://newrelic.com/",
+    },
+    {
+      title: "Skyscanner", //Project Title - Add Your Project Title Here
+      description: "Fullstack engineer",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://raw.githubusercontent.com/jamipuchi/react-portfolio-template/main/public/images/skyscanner.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://skyscanner.com/",
+    },
+    {
+      title: "Booster", //Project Title - Add Your Project Title Here
+      description: "Senior Front end engineer",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://raw.githubusercontent.com/jamipuchi/react-portfolio-template/main/public/images/boosterthon.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.choosebooster.com/events",
+    },
+    {
+      title: "Justinmind", //Project Title - Add Your Project Title Here
+      description: "Front end engineer",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://raw.githubusercontent.com/jamipuchi/react-portfolio-template/main/public/images/justinmind.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://www.justinmind.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        description: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // Services Section ---------------
+
+  services: [
+    {
+      title: "Project lead",
+      description:
+        "I can bring your project to life. I can help you with the planning, the design, the development, the testing and the deployment of your project.",
+    },
+    {
+      title: "Front end engineer",
+      description:
+        "I can build modern apps with React and help you with the development of your website.",
+    },
+    {
+      title: "Fullstack engineer",
+      description:
+        "I can build the backend in Node.js, Express.js, MongoDB and I also have previously worked with Java and Kotlin.",
+    },
+    {
+      title: "Mobile",
+      description:
+        "I've worked with world-class mobile teams, building apps for iOS and Android.",
+    },
+  ],
+
+  // End Services Section --------------------------
+
+  // About Secton --------------
+  aboutpara: `I am a software engineer with a passion for building modern web applications.\n
+     I have a strong background in software engineering and a passion for building products that solve real-world problems.\n
+     I have lots of years of experience in web development and have worked with a wide range of technologies, including React, Node.js, Express.js, MongoDB, and Java.\n
+     I also have a background in mobile development and have worked with a wide range of technologies, including React Native, Android, and iOS.\n
+    Reach out to me to learn how I can help you build your next project.\n`,
+  //   End About Section ---------------------
+  // Hide Blog section
+  hideBlog: true,
+};
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
